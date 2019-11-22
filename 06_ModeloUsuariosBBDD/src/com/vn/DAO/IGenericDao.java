81107179ae9db7b220c6e622e05fa46e08a02e8a
Diego <dieggo554@gmail.com>
2019-11-22 17:28:40 +0100

    -Creado POJO Usuario -Creadas interfaces Generica y Usuario

---
+++ 06_ModeloUsuariosBBDD/src/com/vn/DAO/IGenericDao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.DAO;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author pc
+ * @param <T>
+ */
+public interface IGenericDao<T> {
+    
+    public abstract T crear(T  objetoNuevo) throws Exception;
+    
+    T obtenerPorIndice(int index);
+    
+    T modificar(int index, T objConDatosNuevo) throws Exception;
+    
+    boolean eliminar(int index);
+}
---
+++ 06_ModeloUsuariosBBDD/src/com/vn/POJOs/Usuario.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.POJOs;
+
+/**
+ *
+ * @author pc
+ */
+public class Usuario {
+
+    int id, edad;
+    String nombre, email, password;
+
+    public Usuario(int edad, String nombre, String email, String password) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Usuario(int id, int edad, String nombre, String email, String password) {
+        this.id = id;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario:" + "id=" + id + ", edad=" + edad + ", nombre=" + nombre + ", email=" + email + ", password=" + password;
+    }
+}
