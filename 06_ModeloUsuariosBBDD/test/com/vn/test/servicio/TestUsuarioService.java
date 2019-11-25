129505c79b50e4eb87e0f8787bc487b8b17ed341
Diego <dieggo554@gmail.com>
2019-11-25 11:24:08 +0100

    -Añadido metodo equals a Usuario -Test ampliados

--- 06_ModeloUsuariosBBDD/test/com/vn/test/servicio/TestUsuarioService.java
+++ 06_ModeloUsuariosBBDD/test/com/vn/test/servicio/TestUsuarioService.java
@@ -5,9 +5,10 @@ package com.vn.test.servicio;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import com.vn.POJOs.Usuario;
 import com.vn.servicio.UsuarioServicio;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,45 +17,86 @@ import static org.junit.Assert.*;
  * @author pc
  */
 public class TestUsuarioService {
-    
-     @Test
-     public void testCrearUsuario(Usuario nuevoUsuario) {
-         UsuarioServicio.crear("Eustaquio Habichuela", "correo@corre.com", "abc123.");
-         
-     }
-     
-     @Test
-     public void testLeerUsuarioPorId(Integer id) {
-         
-     }
-     
-     @Test
-     public void testLeerTodosUsuario() {
-         
-     }
-     
-     @Test
-     public void testLeerUsuarioPorEmail(String email) {
-         
-     }
 
-     @Test
-     public void testActualizarPorId(Integer id) {
-         
-     }
-     
-     @Test
-     public void testActualizarPorEmail(Integer email) {
-         
-     }
-     
-     @Test
-     public void testEliminarPorId(Integer id) {
-         
-     }
-     
-     @Test
-     public void testEliminarPorEmail(Integer email) {
-         
-     }
+    final Integer edad;
+    final String nombre, email, password;
+    
+    public TestUsuarioService() {
+        edad = 50;
+        nombre = "Eustaquio Habichuela";
+        email = "correo@corre.com";
+        password = "abc123.";
+    }
+
+    @Test
+    public void testCrear() {
+        Usuario usuario = UsuarioServicio.crear(edad, nombre, email, password);
+        assertEquals(new Usuario(email, password, nombre, edad), usuario);
+    }
+    
+    @Test
+    public void testLeerUsuarioPorId() {
+        Usuario usuario = UsuarioServicio.crear(edad, nombre, email, password);
+        Integer id = usuario.getId();
+        assertEquals(usuario, UsuarioServicio.leer(id));
+    }
+    
+    @Test
+    public void testLeerUsuarioPorEmail() {
+        Usuario usuario = UsuarioServicio.crear(edad, nombre, email, password);
+        assertEquals(usuario, UsuarioServicio.leer(email));
+    }
+    
+    @Test
+    public void testEliminarPorId() {
+        Usuario usuario = UsuarioServicio.crear(edad, nombre, email, password);
+        Integer id = usuario.getId();
+        Boolean eliminado = UsuarioServicio.eliminar(id);
+        assertTrue(eliminado);
+        assertNull(UsuarioServicio.leer(email));
+    }
+    
+    @Test
+    public void testModificarPorId() {
+        Integer id;
+        Usuario usuario = UsuarioServicio.crear(edad, nombre, email, password);
+        id = usuario.getId();
+        Usuario usuarioMod = UsuarioServicio.modificar(id, email, "12345", "Juan", edad);
+        assertEquals("Juan", usuarioMod.getNombre());
+        assertEquals("12345", usuarioMod.getPassword());
+    }
+
+    @Test
+    public void testModificarObjecto() {
+        Integer id;
+        Usuario usuario = UsuarioServicio.crear(edad, nombre, email, password);
+        id = usuario.getId();
+        Usuario usuarioMod = new Usuario(id, email, "12345", "Juan", edad);
+        usuario = UsuarioServicio.modificar(usuarioMod);
+        assertEquals(usuarioMod, usuario);
+    }
+
+    @Test
+    public void testLeerUsuarios() {
+        for (int i = 1; i <= 10; i++) {
+            UsuarioServicio.crear(i, i + "º - " + nombre, i + "º - " + email, i + "º - " + password);
+        }
+        List<Usuario> usuarios = UsuarioServicio.leerTodos();
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario);
+        }
+    }
+    
+    @Test
+    public void testLeerUsuariosPorNombre() {
+        for (int i = 1; i <= 5; i++) {
+            UsuarioServicio.crear(i, i + "º - " + nombre, i + "º - " + email, i + "º - " + password);
+        }
+        UsuarioServicio.crear(50, "Juancho", "emilio@aqui.si", "asdfg");
+        UsuarioServicio.crear(55, "Alfredo", "alfi@dredo.no", "qwerty");
+        List<Usuario> usuarios = UsuarioServicio.leerTodos(nombre);
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario);
+        }
+    }
 }
