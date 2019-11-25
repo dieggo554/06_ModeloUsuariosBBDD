3c1765bfb8316a82207af996639f364c07ca727c
Alberto <bertojaneiro@gmail.com>
2019-11-25 09:35:16 +0100

    -librerias añadidas

--- 06_ModeloUsuariosBBDD/src/com/vn/conexion/ConexionDerby.java
+++ 06_ModeloUsuariosBBDD/src/com/vn/conexion/ConexionDerby.java
@@ -5,10 +5,21 @@
  */
 package com.vn.conexion;
 
+import java.sql.DriverManager;
+
 /** static final de los elementos necesarios para hacer la conexion (url, usuario, contraseña)
  *
  * @author Equipo 1
  */
 public class ConexionDerby {
     //static final 
+    public ConexionDerby(){
+        try{
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+
+        } catch (Exception ex) {
+            System.out.println("no se ha cargado DerbyDB");
+        }
+    }
 }
