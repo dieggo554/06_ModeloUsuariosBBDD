e389bd4f093ba1620de506010f693b62c126e9e9
Alberto <bertojaneiro@gmail.com>
2019-11-25 09:31:15 +0100

    -librerias

---
+++ 06_ModeloUsuariosBBDD/src/com/vn/conexion/ConexionDerby.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.conexion;
+
+/** static final de los elementos necesarios para hacer la conexion (url, usuario, contraseña)
+ *
+ * @author Equipo 1
+ */
+public class ConexionDerby {
+    //static final 
+}
