4db98c115c2c74fc487067da2e0d3f638822f5f3
Diego <dieggo554@gmail.com>
2019-11-26 13:38:21 +0100

    -Comentada linea de UsuarioServicio para devolver null

--- 06_ModeloUsuariosBBDD/src/com/vn/servicio/UsuarioServicio.java
+++ 06_ModeloUsuariosBBDD/src/com/vn/servicio/UsuarioServicio.java
@@ -26,17 +26,6 @@ public class UsuarioServicio {
             Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
             return matcher.find();
     }
-    
-    Pattern patronNombre = Pattern.compile(
-            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)"
-                    + "*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b"
-                    + "\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")"
-                    + "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]"
-                    + "(?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]"
-                    + "?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]"
-                    + "?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-    Pattern patronEmail = Pattern.compile(
-            "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
 
     public static Usuario crear(String edad, String nombre, String email, String contrasena) throws Exception {
         Usuario nuevo = null;
@@ -56,7 +45,7 @@ public class UsuarioServicio {
                         nuevo = new Usuario(email, contrasena, nombre, Integer.parseInt(edad));
                         return dao.crear(nuevo);
                     }
-                    nuevo = dao.obtenerPorEmail(email);
+//                    nuevo = dao.obtenerPorEmail(email);
                     return nuevo;
                 }
             }
